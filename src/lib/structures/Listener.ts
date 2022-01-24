96ffefae91c8ff987e4a2391fc6275ad82fa2946
Nejc Drobnic <ethoarx@gmail.com>
2022-01-24 21:47:49 +0100

    feat: allow ethers filtered events

--- src/lib/structures/Listener.ts
+++ src/lib/structures/Listener.ts
@@ -1,11 +1,12 @@
 // https://github.com/sapphiredev/framework/blob/main/src/lib/structures/Listener.ts
 import { Piece } from '@sapphire/pieces';
+import type { EventFilter } from 'ethers';
 import type { EventEmitter } from 'events';
 import type { Relayer } from '../Relayer';
 
 export abstract class Listener<O extends Listener.Options = Listener.Options> extends Piece<O> {
 	public readonly emitter: EventEmitter | null;
-	public readonly event: string | symbol;
+	public readonly event: string | EventFilter | symbol;
 	public readonly once: boolean;
 
 	private _listener: ((...args: any[]) => void) | null;
@@ -37,7 +38,7 @@ export abstract class Listener<O extends Listener.Options = Listener.Options> ex
 			const maxListeners = emitter.getMaxListeners();
 			if (maxListeners !== 0) emitter.setMaxListeners(maxListeners + 1);
 
-			emitter[this.once ? 'once' : 'on'](this.event, this._listener);
+			emitter[this.once ? 'once' : 'on'](this.event as any, this._listener);
 		}
 		return super.onLoad();
 	}
@@ -50,19 +51,11 @@ export abstract class Listener<O extends Listener.Options = Listener.Options> ex
 			const maxListeners = emitter.getMaxListeners();
 			if (maxListeners !== 0) emitter.setMaxListeners(maxListeners - 1);
 
-			emitter.off(this.event, this._listener);
+			emitter.off(this.event as any, this._listener);
 			this._listener = null;
 		}
 
 		return super.onUnload();
-	}
-
-	public toJSON(): ListenerJSON {
-		return {
-			...super.toJSON(),
-			once: this.once,
-			event: this.event
-		};
 	}
 
 	private async _run(...args: unknown[]) {
@@ -81,17 +74,11 @@ export abstract class Listener<O extends Listener.Options = Listener.Options> ex
 
 export interface ListenerOptions extends Piece.Options {
 	readonly emitter?: keyof Relayer | EventEmitter;
-	readonly event?: string | symbol;
+	readonly event?: string | EventFilter | symbol;
 	readonly once?: boolean;
-}
-
-export interface ListenerJSON extends Piece.JSON {
-	event: string | symbol;
-	once: boolean;
 }
 
 export namespace Listener {
 	export type Options = ListenerOptions;
-	export type JSON = ListenerJSON;
 	export type Context = Piece.Context;
 }
